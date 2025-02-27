92b9aede10bb6ebfd23929e8d78d7d44219c0923
AndrePereira123 <147044831+AndrePereira123@users.noreply.github.com>
2025-02-27 16:13:02 +0000

    listas a ser convertidas

--- TPC3/Conversor.py
+++ TPC3/Conversor.py
@@ -1,23 +1,45 @@
 import re
 
-Padrao_Regex = '^.*?;.*?;\d{4};.*?;.*?;\d\d:\d\d:\d\d;\d*'
-
-
-
 def main():
-    f = open("obras.csv", "r",encoding = "utf-8")
+    f = open("Original.md", "r",encoding = "utf-8")
     print(f"\nDados do ficheiro \"{f.name}\" serão processados agora\n")
-    Lista_Compositores = []
-    N_Obras_por_periodo = {}
-    Obras_por_periodo = {}
     ##procurar 6 instancias de ; antes de fazer split - dado q existe sempre um \n entre instancias podemos faze lo
-    instancia_em_texto = ""
-    next(f) ##ignorar primeira linha
-    for linha in f:
-        instancia_em_texto += linha
-        if (re.match(Padrao_Regex,instancia_em_texto,re.DOTALL)):
-            guardar_informacao(instancia_em_texto,Lista_Compositores,N_Obras_por_periodo,Obras_por_periodo)
-            instancia_em_texto = ""
+    html = f.read()
+
+    pagHTML = "<!DOCTYPE html>  <html> <head> <style> body { background-color: #121212; color: #ffffff; text-align:center;font-size:2rem;} </style> </head>"
+
+
+    html = re.sub(r"#(\s+.+)",r"<h1>\1</h1>",html)
+    html = re.sub(r"\*\*(.+?)\*\*",r"<b>\1</b>",html)
+    html = re.sub(r"\*(.+?)\*",r"<i>\1</i>",html)
+    
+    padrao_listas = r"((?:\d+\.\s[^\n]+\n)+)"
+    
+    ##html = re.sub(r"(\d+)\.\s(.+)(\d+)\.\s([^\n]+)", r"<ol><li>\2</li></ol>", html ,flags=re.DOTALL)
+
+
+    # Step 1: Extract matches
+    listas = re.findall(padrao_listas,html,flags=re.DOTALL)
+
+    listas_em_html = []
+    for lista in listas:
+        lista_html = ""
+        for linha in lista.split("\n"):
+            if len(linha) > 3:
+                print(linha)
+                lista_html += ("<li>" + linha[3:] + "</li>")
+        listas_em_html.append("<ol>" + lista_html + "</ol>")
+
+
+    for original in listas:
+        html = html.replace(original, listas_em_html.pop())
+
+
+    pagHTML += html
+
+    with open("output.html", "w", encoding="utf-8") as html_file:
+        html_file.write(pagHTML)
+    
     print("Processamento completo!\n")
 
 
---
+++ TPC3/Original.md
@@ -0,0 +1,25 @@
+# PL2025-A104275 || André Filipe Soares Pereira || A104275
+# TPC-3: Conversor MarkDown para HTML
+
+Texto com teste de **bold** com *italico* e tudo mais para ver se *funciona* com as **novas** coisa que foram de *facto* **implementadas**
+
+1. prim item
+2. Segundo item
+3. Terceiro item
+
+
+1. seg item
+2. Segundo item
+3. Terceiro item
+
+Texto com teste de **bold** com *italico* e tudo mais para ver se *funciona* com as **novas** coisa que foram de *facto* **implementadas**
+
+1. prim item
+2. Segundo item
+3. Terceiro item
+
+
+1. seg item
+2. Segundo item
+3. Terceiro item
+
