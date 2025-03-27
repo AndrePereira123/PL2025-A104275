9bc1164cea594049dc7a15b5619b098ed4093673
AndrePereira123 <147044831+AndrePereira123@users.noreply.github.com>
2025-03-27 14:57:17 +0000

    anterior estava muito errado correcao do algoritmo e prints minimos

--- TPC6/parser LL(1).py
+++ TPC6/parser LL(1).py
@@ -30,14 +30,14 @@ def rec_FATOR():
     if prox_simb.type == 'NUM':
         valor = int(prox_simb.value)
         rec_term('NUM')
-        print("Reconheci P4: Fator --> Num")
+        print("Reconheci P7: Fator --> Num ", valor)
         return valor
     elif prox_simb.type == 'LPAR':
-        print("Derivando por P4: Fator --> '(' Exp ')'")
+      ##   print("Derivando por P8: Fator --> '(' Exp ')'")
         rec_term('LPAR')
         valor = rec_expressao()
         rec_term('RPAR')
-        print("Reconheci P4: Fator --> '(' Exp ')'")
+        print("Reconheci P8: Fator --> '(' Exp ')'")
         return valor
     else:
         parserError(prox_simb)
@@ -45,28 +45,35 @@ def rec_FATOR():
 
 
 def rec_TERMO():
+  ##   print("Derivando por P4: Termo --> Fator Termo2")
     valor = rec_FATOR()
     while prox_simb and prox_simb.type == 'TIMES':
-            print("Derivando por P3: Termo --> Fator * Fator")
+       ##      print("Derivando por P5: Termo2 --> * Termo")
             rec_term('TIMES')
             valor *= rec_FATOR()
-            print("Reconheci P3: Termo --> Fator * Fator")
+            print("Reconheci P5: Termo2 --> * Termo")
+    ## print("Reconheci P6: Termo2 --> ''")
+    ## print("Reconheci P4: Termo --> Fator Termo2")
     return valor
 
 
 def rec_expressao():
+   ##  print("Derivando por P0: Exp --> Termo Exp2")
     valor = rec_TERMO()
     while prox_simb and prox_simb.type in ['PLUS', 'MINUS']:
         if prox_simb.type == 'PLUS':
-            print("Derivando por P1: Exp --> Termo + Termo")
+      ##       print("Derivando por P1: Exp2 --> '+' Exp")
             rec_term('PLUS')
             valor += rec_TERMO()
-            print("Reconheci P1: Exp --> Termo + Termo")
+            print("Reconheci P1: Exp2 --> '+' Exp")
         elif prox_simb.type == 'MINUS':
-            print("Derivando por P2: Exp --> Termo - Termo")
+     ##        print("Derivando por P2: Exp2 --> '-' Exp")
             rec_term('MINUS')
             valor -= rec_TERMO()
-            print("Reconheci P2: Exp --> Termo - Termo")
+            print("Reconheci P2: Exp2 --> '-' Exp")
+    
+   ## print("Reconheci P3: Exp2 --> ''")
+    print("Reconheci P0: Exp --> Termo Exp2")
     return valor
 
 
